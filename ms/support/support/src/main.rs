63093b6adfd3c43bd6b620428080cd01b3f20e53
sRRRs <srrrs4510@gmail.com>
2024-12-22 20:38:28 +0900

    [cosan] rust support api implement

--- ms/support/support/src/main.rs
+++ ms/support/support/src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+mod router {
+    pub mod router;
+}
+
+use crate::router::router::new_router;
+
+#[tokio::main]
+async fn main() {
+    new_router().await;
 }
--- ms/support/support/src/router/router.rs
+++ ms/support/support/src/router/router.rs
@@ -1,19 +1,23 @@
 use axum::{
     routing::get,
     Router,
+    Server,
+    http::StatusCode,
 };
+use std::net::SocketAddr;
+
+async fn health_check() -> Result<&'static str, StatusCode> {
+    Ok("OK")
+}
 
 pub async fn new_router() {
-    let router = Router::new();
+    let router = Router::new().route("/", get(health_check));
 
-    router.route("/", get(health_check));
+    let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
+    println!("Listening on {}", addr);
 
-    axum::Server::bind(&"0.0.0.0:8080".parse().unwrap())
+    Server::bind(&addr)
         .serve(router.into_make_service())
         .await
         .unwrap();
-}
-
-fn health_check() -> &'static str {
-    "OK"
 }
