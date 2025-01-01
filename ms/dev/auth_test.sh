1841e7e898b3c71fd0c61bfefdf37138226f0be0
sRRRs <srrrs4510@gmail.com>
2025-01-01 23:32:31 +0900

    [cosan] auth api implement

--- ms/dev/auth_test.sh
+++ ms/dev/auth_test.sh
@@ -6,5 +6,6 @@ ci="ci"
 if [ "$mode" = "$ci" ]; then
     cd /go/ms/auth/src && go test -v ./...
 else 
+    mkdir -p /go/ms/dev/coverage
     cd /go/ms/auth/src && go test -v ./... -coverprofile=/go/ms/dev/coverage/auth_coverage.out && go tool cover -html=/go/ms/dev/coverage/auth_coverage.out -o /go/ms/dev/coverage/auth.html
 fi
